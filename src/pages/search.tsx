5fb43e9c5c0064ef4e756d95a487d75de383e76a
tudahe160975 <tudahe160975@fpt.edu.vn>
2023-06-11 23:43:42 +0700

    fix: Fitler Produtcs

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,6 +1,5 @@
 import { useParams } from "react-router-dom";
 import {useEffect, useState} from "react"
-import ListBrand from "./product/ListBrand";
 import ListProduct from "./product/ListProduct";
 
 const SearchPage = () => {
